1ba929641e2f5df8545f4c75bb5657ed3e160174
Unknown <panuphan1997@gmail.com>
2018-03-31 01:28:17 +0700

    Test Insert API

--- TestP.py
+++ TestP.py
@@ -15,22 +15,46 @@ userData = db['userData']
 #json_data = json.load(open('primer-dataset.json')) 
 #print(json.dumps(json_data,sort_keys=True,indent=2)) 
 
+#Response login
+Response = {
+            "status":"success",
+            "cookie_session":"KFIOGMREOIVK439050I",
+            "admin":"yes"
+}
+
 
 def get(post_id):
     # Convert from string to ObjectId:
     document = client.db.collection.find_one({'_id': ObjectId(post_id)}) 
 
 def insert(room,Name,date,start,end):
-    room = room["available-room"]
     data = {
-            'room': room,
-            'username':Name,
-            'Datetime':{'date':date,
-                        'start': start,
-                        'End': end
-                        }
-    }
+            "available-room": [
+                {
+                "Room": "6272",
+                "schedule": [
+                    {
+                    "Username": "narit",
+                    "Data_Time": "25/4/2561 12:00-16:00"
+                    },
+                    {
+                    "Username": "narit",
+                    "Data Time": "28/4/2561 12:00-16:00"
+                    }
+                ]
+                },
+                {
+                "Room": "6273",
+                "schedule": []
+                },
+                {
+                "Room": "6274",
+                "schedule": []
+                }
+            ]
+            }
     room.insert_one(data)
+
 
 def insert(username,password):
     data = {
@@ -39,11 +63,23 @@ def insert(username,password):
     }
     userData.insert_one(data)
 
+def insert(JSON):
+    userData.insert_one(JSON)
+
 
 
 #main
+import requests
+r = requests.get('serverport')
+jsondata = json.loads( r.json() )
+username = jsondata['username']
+#username = json.loads(r.json())['username']
 
-insert('narit1',77624)
+if userData.find_one({'username' : username}) == None:
+    insert(r.json)
+    r = requests.post(url, Response)
+
+
 
 
 
