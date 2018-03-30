9b9c69619027774604fe02c33dd0d1e102452e1b
Unknown <panuphan1997@gmail.com>
2018-03-30 23:58:49 +0700

    test insert method

--- TestP.py
+++ TestP.py
@@ -6,23 +6,45 @@ from bson.objectid import ObjectId
 
 #Variable
 URI = "mongodb+srv://root:root@egco231-ettdb.mongodb.net"
-json_data = json.load(open('primer-dataset.json')) 
+client = MongoClient(URI)
+db       = client['EGCO231']
+room     = db['Room']
+userData = db['userData']
+
+
+#json_data = json.load(open('primer-dataset.json')) 
 #print(json.dumps(json_data,sort_keys=True,indent=2)) 
 
 
 def get(post_id):
     # Convert from string to ObjectId:
-    document = client.db.collection.find_one({'_id': ObjectId(post_id)})
+    document = client.db.collection.find_one({'_id': ObjectId(post_id)}) 
+
+def insert(room,Name,date,start,end):
+    room = room["available-room"]
+    data = {
+            'room': room,
+            'username':Name,
+            'Datetime':{'date':date,
+                        'start': start,
+                        'End': end
+                        }
+    }
+    room.insert_one(data)
+
+def insert(username,password):
+    data = {
+            'username':username,
+            'password':password
+    }
+    userData.insert_one(data)
+
 
 
 #main
 
-client = MongoClient(URI)
-db  = client['EGCO231']
-userData = db['userData']
-Rooms = db['Room']
+insert('narit1',77624)
 
-userData.insert_one(json_data)
 
 
 
