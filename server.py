09815a2d19368603b57421b61e69b016d351857c
Unknown <sataporn.san@student.mahidol.ac.th>
2018-03-31 15:46:03 +0700

    รอเพื่อน

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 #import
 
-
+from flask import Flask, url_for,Response,request,json
 
 # ////////////////////////////////////////////////////////
 # load config
@@ -37,7 +37,61 @@
 # ///////////////////////////////////////////////////////
 # Route
 
-afsdsadsa
+app = Flask(__name__)
+
+@app.route('/login', methods = ['POST'])
+def api_login():
+    
+    if request.method =='POST':
+       
+            if request.headers['Content-Type'] == 'application/json':
+                return "JSON Message: " + json.dumps(request.json)
+            else:
+                return "415 Unsupported Media Type ;)"
+    else: return "fail_POST_LOGIN"
+
+@app.route('/regis', methods = ['POST'])
+def api_regis():
+    
+    if request.method =='POST':
+       
+            if request.headers['Content-Type'] == 'application/json':
+                return "JSON Message: " + json.dumps(request.json)
+            else:
+                return "415 Unsupported Media Type ;)"
+    else: return "fail_POST_REGIS"
+
+@app.route('/book', methods = ['POST'])
+def api_book():
+    
+    if request.method =='POST':
+       
+            if request.headers['Content-Type'] == 'application/json':
+                return "JSON Message: " + json.dumps(request.json)
+            else:
+                return "415 Unsupported Media Type ;)"
+    else: return "fail_POST_BOOK"
+
+@app.route('/cancel', methods = ['POST'])
+def api_cancel():
+    
+    if request.method =='POST':
+       
+            if request.headers['Content-Type'] == 'application/json':
+                return "JSON Message: " + json.dumps(request.json)
+            else:
+                return "415 Unsupported Media Type ;)"
+    else: return "fail_POST_CANCEL"
+@app.route('/list', methods = ['GET'])
+def api_list():
+    
+    if request.method =='GET': return "list of Room"      
+    else: return "fail_POST_CANCEL"
+
+       
+if __name__ == '__main__':
+    app.run()
+
 
 
 # ///////////////////////////////////////////////////////
