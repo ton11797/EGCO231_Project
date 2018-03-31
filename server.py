236079d3c392b50236be4774043516aab2859d96
SwitzZ <supavitwork@gmail.com>
2018-03-31 14:07:58 +0700

    Auto stash before merge of "ConnectDatabase" and "origin/Connect_Database"

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 #import
+from pymongo import MongoClient
 
 #Hello word
 
@@ -12,8 +13,26 @@
 
 # ///////////////////////////////////////////////////////
 # database
-
-
+class database:
+	def __init__(self):
+		self.client = MongoClient('localhost', 27017)
+		self.db = self.client['EGCO']
+	def have_user(self,username):
+		self.user = self.db['userData']
+		if str(self.user.find_one({"username":username})) == "None":
+			return False
+		else:
+			return True
+	def get_room(self):
+		return self.
+	def login(self,username,password):
+		self.user = self.db['userData']
+		if str(self.username)==str(self.user.f) and str(self.password)==str(self.db['password']):
+			return True
+		else :
+			return False
+DB = database()
+print(DB.have_user("admin"))
 # ///////////////////////////////////////////////////////
 # book
 
@@ -25,7 +44,7 @@
 # ///////////////////////////////////////////////////////
 # cancel
 
-asdas
+
 # ///////////////////////////////////////////////////////
 # register
 
