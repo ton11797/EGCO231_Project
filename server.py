5659e2043086f4b0a848633f22cb22967b56664f
SwitzZ <supavitwork@gmail.com>
2018-03-31 16:03:38 +0700

    clean

--- server.py
+++ server.py
@@ -112,57 +112,44 @@ app = Flask(__name__)
 
 @app.route('/login', methods = ['POST'])
 def api_login():
-    
     if request.method =='POST':
-       
-            if request.headers['Content-Type'] == 'application/json':
-                return "JSON Message: " + json.dumps(request.json)
-            else:
-                return "415 Unsupported Media Type ;)"
-    else: return "fail_POST_LOGIN"
+		if request.headers['Content-Type'] == 'application/json':
+			return "JSON Message: " + json.dumps(request.json)
+		else:
+			return "415 Unsupported Media Type ;)"
+	else: return "fail_POST_LOGIN"
 
 @app.route('/regis', methods = ['POST'])
 def api_regis():
-    
-    if request.method =='POST':
-       
-            if request.headers['Content-Type'] == 'application/json':
-                return "JSON Message: " + json.dumps(request.json)
-            else:
-                return "415 Unsupported Media Type ;)"
-    else: return "fail_POST_REGIS"
+	if request.method =='POST':
+		if request.headers['Content-Type'] == 'application/json':
+			return "JSON Message: " + json.dumps(request.json)
+		else:
+			return "415 Unsupported Media Type ;)"
+	else: return "fail_POST_REGIS"
 
 @app.route('/book', methods = ['POST'])
 def api_book():
-    
-    if request.method =='POST':
-       
-            if request.headers['Content-Type'] == 'application/json':
-                return "JSON Message: " + json.dumps(request.json)
-            else:
-                return "415 Unsupported Media Type ;)"
-    else: return "fail_POST_BOOK"
+	if request.method =='POST':
+		if request.headers['Content-Type'] == 'application/json':
+				return "JSON Message: " + json.dumps(request.json)
+		else:
+			return "415 Unsupported Media Type ;)"
+	else: return "fail_POST_BOOK"
 
 @app.route('/cancel', methods = ['POST'])
 def api_cancel():
-    
-    if request.method =='POST':
-       
-            if request.headers['Content-Type'] == 'application/json':
-                return "JSON Message: " + json.dumps(request.json)
-            else:
-                return "415 Unsupported Media Type ;)"
-    else: return "fail_POST_CANCEL"
+	if request.method =='POST':
+		if request.headers['Content-Type'] == 'application/json':
+				return "JSON Message: " + json.dumps(request.json)
+		else:
+			return "415 Unsupported Media Type ;)"
+	else: return "fail_POST_CANCEL"
 @app.route('/list', methods = ['GET'])
 def api_list():
-    
-    if request.method =='GET': return "list of Room"      
-    else: return "fail_POST_CANCEL"
-
-       
+	if request.method =='GET': return "list of Room"
+	else: return "fail_POST_CANCEL"
 if __name__ == '__main__':
-    app.run()
-
-
+	app.run()
 
 # ///////////////////////////////////////////////////////
