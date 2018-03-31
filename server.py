82d459bd43e5d139cead53badc686152d4dfe1a8
Unknown <panuphan1997@gmail.com>
2018-03-31 13:02:05 +0700

    Update check Book

--- server.py
+++ server.py
@@ -1,12 +1,9 @@
 #import
-import pprint 
-import json 
-from bson.objectid import ObjectId 
-from flask import Flask, request 
-from flask_restful import Resource, Api 
-from sqlalchemy import create_engine 
-from json import dumps 
-from flask.ext.jsonpify import jsonify 
+from pymongo import MongoClient
+import datetime
+import pprint
+import json
+
 
 #Variable 
 URI = "mongodb+srv://root:root@egco231-ettdb.mongodb.net" 
@@ -14,6 +11,7 @@ client = MongoClient(URI)
 db       = client['EGCO231'] 
 room     = db['Room'] 
 userData = db['userData'] 
+
 
 
 # ////////////////////////////////////////////////////////
@@ -30,6 +28,32 @@ userData = db['userData']
 
 # ///////////////////////////////////////////////////////
 # book
+def get_room():
+    json_data = json.load(open('EGCO231_getroom.json')) 
+    return json_data['available-room']
+#print(json.dumps(get_room(),sort_keys=True,indent=2))
+
+def checkBook(rm,date):
+    for r in get_room() :
+        #print(r['Room'])
+        #print(r['schedule'])
+        if rm == r['Room'] :
+            for s in r['schedule']:
+                #print(s['Data_Time'])
+                dateTime = s['Data_Time'].split()
+                
+                if dateTime == d:
+                    return False
+                else:
+                    time = dateTime[1].split('-')
+
+                
+checkBook('6272','25/4/2561 12:00-16:00')
+    
+
+
+ 
+        
     
 
 
