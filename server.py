0774a3e5edde1c4922eec967e2fa5f3b4e605a50
SwitzZ <supavitwork@gmail.com>
2018-03-31 15:39:52 +0700

    get_room login register

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 #import
 from pymongo import MongoClient
-
+import random
+import string
+import json
 #Hello word
 
 # ////////////////////////////////////////////////////////
@@ -23,16 +25,57 @@ class database:
 			return False
 		else:
 			return True
+
+	def register(self,username,password):
+		if self.have_user(username):
+			respond = {"status":"Fail","error":"username exist"}
+			return json.dumps(respond)
+		else:
+			self.user.insert_one({"username":username,"password":password})
+			respond = {"status":"sucess","error":"username exist"}
+			return json.dumps(respond)
+
+	def rand_string(self,length, char_set=10):
+		output = ''
+		for _ in range(length): output += random.choice(char_set)
+		return output
+
 	def get_room(self):
-		return self.
+		self.room = self.db['room']
+		collect = self.room.find({})
+		array =[]
+		for c in collect:
+			del c['_id']
+			array.append(c)
+		respond = {"available-room":array}
+		return json.dumps(respond)
+
+	def genCookies(self,username):
+		self.session = self.db['loginSession ']
+		self.random = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(32)])
+		self.session.insert_one({"username":username,"cookies":self.random})
+		return self.random
+
 	def login(self,username,password):
 		self.user = self.db['userData']
-		if str(self.username)==str(self.user.f) and str(self.password)==str(self.db['password']):
-			return True
-		else :
-			return False
+		if self.user.find_one({"username":username,"password":password})=="None":
+			respond = {"status":"Fail","error":"username or password not match"}
+			return json.dumps(respond)
+		else:
+			self.cookies = self.genCookies(username)
+			self.admin = "no"
+			if username=="admin":
+				self.admin = "yes"
+			respond = {"status":"sucess","error":"none","cookie_session":self.cookies,"admin":self.admin}
+			return json.dumps(respond)
+
+
+
 DB = database()
-print(DB.have_user("admin"))
+# print(DB.have_user("admin"))
+# print(DB.get_room())
+print(DB.login("admin","123"))
+
 # ///////////////////////////////////////////////////////
 # book
 
