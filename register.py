af02212a3b0cc0da39216bcbfa69e5c3d564c002
VEARTH <earth14828@gmail.com>
2018-03-31 12:55:22 +0700

    regis

---
+++ register.py
@@ -0,0 +1,10 @@
+import json
+
+def Register(input):
+    data = json.loads(input)
+    if(have_user(data["Username"]):
+        return json.dumps('{"status":"fail","error":"This username is already in use"')
+    else:
+        insert(json.dumps("{\"Username\":"+data["Username"]+",\"Password\":"+data["Password"]))
+        return json.dumps('{"status":"success","error":"none"')
+
