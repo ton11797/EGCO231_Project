ff8d9a956940df7a84ab42c9956206d39d130e30
Unknown <por_25418@hotmail.com>
2018-04-04 14:57:47 +0700

    ทำfunction search_room+เอาค่าจากcomboboxมาใช้

--- User client/search_room.py
+++ User client/search_room.py
@@ -7,49 +7,73 @@ class home(Tk):
         mainFrame = Frame(self,width=500, height=400, bd=1, relief=SUNKEN)
         
         Button(menuList, text='จองห้อง',relief="flat",padx=39).pack(side=TOP, padx=5)
-        Button(menuList, text='ตรวจสอบห้องว่าง',relief="flat",padx=19,command = lamdba:self.search_room(mainFrame)).pack(side=TOP, padx=5)
+        Button(menuList, text='ตรวจสอบห้องว่าง',relief="flat",padx=19,command = lambda:self.search_room(mainFrame)).pack(side=TOP, padx=5)
         Button(menuList, text='ตารางการใช้ห้อง',relief="flat",padx=20).pack(side=TOP, padx=5)
         menuList.pack( side=LEFT,fill=Y,pady=10, padx=5)
         mainFrame.pack(side=LEFT,expand=1, pady=10, padx=5)
-
+        
+    def search_list_room(self,mainFrame,combo1,combo2,combo3,combo4,combo5):
+        label1 = Label(mainFrame,text = "ห้อง = "+combo1.get())
+        label1.place(x=5,y=90)
+        label2 = Label(mainFrame,text = "วัน = "+combo2.get())
+        label2.place(x=5,y=110)
+        label3 = Label(mainFrame,text = "เดือน = "+combo3.get())
+        label3.place(x=5,y=130)
+        label4 = Label(mainFrame,text = "ปี = "+combo4.get())
+        label4.place(x=5,y=150)
+        label5 = Label(mainFrame,text = "เวลา = "+combo5.get())
+        label5.place(x=5,y=170)
+        
+        print(combo1.get()+"  ")
+        print(combo2.get()+"  ")
+        print(combo3.get()+"  ")
+        print(combo4.get()+"  ")
+        print(combo5.get()+"  ")
+        print("\n")
+        
+        
     def search_room(self,mainFrame):
         label = Label(mainFrame,text ="ห้อง : ")
         label.place(x=5,y=10)
         label = Label(mainFrame,text ="วัน : ")
-        label.place(x=85,y=10)
+        label.place(x=90,y=10)
         label = Label(mainFrame,text ="เดือน : ")
-        label.place(x=155,y=10)
+        label.place(x=170,y=10)
         label = Label(mainFrame,text ="ปี : ")
-        label.place(x=255,y=10)
+        label.place(x=290,y=10)
         label = Label(mainFrame,text ="เวลา : ")
-        label.place(x=305,y=10)
+        label.place(x=365,y=10)
         #ห้อง
         combo1 = ttk.Combobox(mainFrame,width=5)
-        combo1.place(x=28,y=10)
-        combo1['value'] = ('R321')
+        combo1.place(x=35,y=10)
+        combo1['value'] = ('R231')
         #วัน
-        combo2 = ttk.Combobox(mainFrame,width=5)
-        combo2.place(x=95,y=50)
-        combo2['value'] = ('1','','','','','','','','','','','','','','','','','','','','','','','','','','')
-        
-        #for i in 32:
-        #    combo2['value'] = i
-        
+        i = 0
+        day  = list();
+        while (i < 31):
+            day.append(i+1)
+            i = i + 1
+        combo2 = ttk.Combobox(mainFrame,width=5,value = day)
+        combo2.place(x=115,y=10)
         #เดือน
         combo3 = ttk.Combobox(mainFrame,width=10)
-        combo3.place(x=265,y=10)
-        combo3['value'] = ('มกราคม','กุมพาพันธ์','มีนาคม','เมษายน','พฤษภาคม','มิถุนายน','กรกฎาคม','สิงหาคม','กันยายน','ตุลาคม','พฤศจิกายน','ธันวาคม')
+        combo3.place(x=205,y=10)
+        combo3['value'] = ('มกราคม','กุมภาพันธ์','มีนาคม','เมษายน','พฤษภาคม','มิถุนายน','กรกฎาคม','สิงหาคม','กันยายน','ตุลาคม','พฤศจิกายน','ธันวาคม')
         #ปี
-        combo3 = ttk.Combobox(mainFrame,width=5)
-        combo3.place(x=285,y=5)
-        combo3['value'] = ('2559','2560','2561','2562','2563','2564','2565','2566','2567')
+        i = 2549
+        year  = list();
+        while (i < 2561):
+            year.append(i+1)
+            i = i + 1
+        combo4 = ttk.Combobox(mainFrame,width=5,value = year)
+        combo4.place(x=310,y=10)
         #เวลา
-        combo4 = ttk.Combobox(mainFrame,width=10)
-        combo4.place(x=340,y=10)
-        combo4['value'] = ('07.00-08.00','08.00-09.00','09.00-10.00','10.00-11.00','11.00-12.00','13.00-14.00','14.00-15.00','15.00-16.00','16.00-17.00','17.00-18.00','18.00-19.00','19.00-20.00','20.00-21.00','21.00-22.00')
+        combo5 = ttk.Combobox(mainFrame,width=10)
+        combo5.place(x=400,y=10)
+        combo5['value'] = ('07.00-08.00','08.00-09.00','09.00-10.00','10.00-11.00','11.00-12.00','13.00-14.00','14.00-15.00','15.00-16.00','16.00-17.00','17.00-18.00','18.00-19.00','19.00-20.00','20.00-21.00','21.00-22.00')
         
+        boton = Button(mainFrame,text = 'ค้นหา',command = lambda:self.search_list_room(mainFrame,combo1,combo2,combo3,combo4,combo5)).place(x=230,y=50)
 
-        
         # run
         self.mainloop()
 
